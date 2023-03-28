ce418062d370405b918cdc234cb11ff157f66e74
Samanta Podgórska <117597986+Samcia@users.noreply.github.com>
2023-03-28 08:57:03 +0200

    Add files via upload

--- js/task-10.js
+++ js/task-10.js
@@ -1,28 +1,44 @@
-const createEl = document.querySelector("[data-create]");
-const inputEl = document.querySelector("#controls input");
-const boxesEl = document.getElementById("boxes");
-
-let counter = 0;
-
-createEl.addEventListener("click", () => {
-  console.log("creating boxes...");
-
-  boxesEl.append(...createBoxes(inputEl.value));
-});
-
-function createBoxes(amount) {
-  const boxes = [];
-
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-
-    box.classList.add("box");
-    box.style.height = `${30 + counter * 10}px`;
-    box.style.width = `${30 + counter * 10}px`;
-
-    counter++;
-    boxes.push(box);
-  }
-
-  return boxes;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const divBox = document.querySelector("#boxes");
+const firstdiv = document.createElement("div");
+
+let size = 20;
+
+firstdiv.style.backgroundColor = getRandomHexColor();
+
+const getInputValue = () => {
+  for (let i = 0; i < document.querySelector("input").value; i++) {
+    const divEl = document.createElement("div");
+    divBox.append(divEl);
+    divEl.setAttribute(
+      "style",
+      "width: " +
+        (size += 10) +
+        "px;" +
+        " height: " +
+        size +
+        "px;" +
+        " background-color: " +
+        getRandomHexColor()
+    );
+  }
+};
+
+const removeAct = () => {
+  const list = document.getElementById("boxes");
+  const input = document.querySelector('input[type="number"]');
+  // while (list.hasChildNodes()) {
+  //   list.removeChild(list.firstChild);
+  list.innerHTML = "";
+  size = 20;
+  input.value = "";
+};
+
+createBtn.addEventListener("click", getInputValue);
+destroyBtn.addEventListener("click", removeAct);
