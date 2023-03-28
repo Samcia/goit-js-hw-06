32d435ad65733c18b78ef66966839a40984b5e66
Samanta Podgórska <117597986+Samcia@users.noreply.github.com>
2023-03-28 08:44:35 +0200

    Add files via upload

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,28 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const createEl = document.querySelector("[data-create]");
+const inputEl = document.querySelector("#controls input");
+const boxesEl = document.getElementById("boxes");
+
+let counter = 0;
+
+createEl.addEventListener("click", () => {
+  console.log("creating boxes...");
+
+  boxesEl.append(...createBoxes(inputEl.value));
+});
+
+function createBoxes(amount) {
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+
+    box.classList.add("box");
+    box.style.height = `${30 + counter * 10}px`;
+    box.style.width = `${30 + counter * 10}px`;
+
+    counter++;
+    boxes.push(box);
+  }
+
+  return boxes;
 }
